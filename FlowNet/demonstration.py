7bc25d81a8450bfff258e1386370aa409854f825
Bobby-0228 <k970244.ee09@nycu.edu.tw>
2024-06-10 19:58:15 +0800

    show both model_c and model_s in demonstration

--- FlowNet/demonstration.py
+++ FlowNet/demonstration.py
@@ -7,19 +7,23 @@ import torch.backends.cudnn as cudnn
 from imageio.v2 import imread, imwrite
 from flow_estimate import calcOpticalFlowFlownet
 
-network_data = torch.load("/home/cv/Projects/FlowNetPytorch/copy/model_s_best.pth.tar")
+device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+cudnn.benchmark = True
 model_names = sorted(
     name for name in models.__dict__ if name.islower() and not name.startswith("__")
 )
-# cap = cv.VideoCapture("./cat/cat.mp4")
-device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
-model = models.__dict__[network_data["arch"]](network_data).to(device)
-model.eval()
-cudnn.benchmark = True
 
-if "div_flow" in network_data.keys():
+network_s_data = torch.load("/home/cv/Projects/FlowNetPytorch/copy/model_s_best.pth.tar")
+model_s = models.__dict__[network_s_data["arch"]](network_s_data).to(device)
+model_s.eval()
+
+network_c_data = torch.load("/home/cv/Projects/FlowNetPytorch/copy/model_c_best.pth.tar")
+model_c = models.__dict__[network_c_data["arch"]](network_c_data).to(device)
+model_c.eval()
+
+if "div_flow" in network_s_data.keys():
     # what is this??
-    div_flow = network_data["div_flow"]
+    div_flow = network_s_data["div_flow"]
 else:
     div_flow = 20.0
 
@@ -30,9 +34,12 @@ while(1):
     if not ret:
         print('No frames grabbed!')
         break
-    flow = calcOpticalFlowFlownet(model, frame1, frame2, div_flow, upsampling=None)
-    cv.imshow("Optical Flow by FlowNet", cv.cvtColor(flow, cv.COLOR_RGB2BGR) )
+    flow = calcOpticalFlowFlownet(model_s, frame1, frame2, div_flow, upsampling=None)
+    flow = calcOpticalFlowFlownet(model_c, frame1, frame2, div_flow, upsampling=None)
+    cv.imshow("Optical Flow by FlowNet_s", cv.cvtColor(flow, cv.COLOR_RGB2BGR) )
+    cv.imshow("Optical Flow by FlowNet_c", cv.cvtColor(flow, cv.COLOR_RGB2BGR) )
     cv.imshow("Camera Sight", frame2)
+
     k = cv.waitKey(30) & 0xff
     if k == 27:
         break
